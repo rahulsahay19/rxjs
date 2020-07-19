684436ed718817d3a8aeddb57e2a9f65e71da1ab
Rahul Sahay <rahulsahay19@hotmail.com>
2020-07-20 01:05:24 +0530

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,17 +14,26 @@ export class AppComponent implements OnInit {
     //Observable of number
     const interval$ = interval(3000);
 
-    interval$.subscribe(value => console.log(`1st Stream:- ${value}`));
-    interval$.subscribe(value => console.log(`2nd Stream:- ${value}`));
-    interval$.subscribe(value => console.log(`3nd Stream:- ${value}`));
+    const sub1 = interval$.subscribe(value => console.log(`1st Stream:- ${value}`));
+    const sub2 = interval$.subscribe(value => console.log(`2nd Stream:- ${value}`));
+    const sub3 = interval$.subscribe(value => console.log(`3nd Stream:- ${value}`));
 
     const interval2$ = timer(3000, 1000);
-    interval2$.subscribe(value => console.log(`timer Stream:- ${value}`));
+    const sub4 = interval2$.subscribe(value => console.log(`timer Stream:- ${value}`));
 
     const clickStream$ = fromEvent(document, 'click');
-    clickStream$.subscribe(
+    const sub5 = clickStream$.subscribe(
       value => console.log(`Click stream:-${value}`),
       error => console.log(`Error Occured:${error}`),
       () => console.log(`stream completed`));
+
+    setTimeout(() => {
+      sub1.unsubscribe();
+      sub2.unsubscribe();
+      sub3.unsubscribe();
+      sub4.unsubscribe();
+      sub5.unsubscribe();
+      console.log('unsubscribed')
+    }, 5000);
   }
 }
