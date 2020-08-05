4b262d44075cab8defdb17ff8430836cdf527ad9
Rahul Sahay <rahulsahay19@hotmail.com>
2020-08-05 11:58:35 +0530

    more changes

--- server/get-movies.route.ts
+++ server/get-movies.route.ts
@@ -7,21 +7,11 @@ import { MOVIES } from "./db-movie";
 
 export function getAllMovies(req: Request, res: Response) {
 
-    /*
-       const error = (Math.random() >= 0.5);
-       if (error) {
-           console.log("ERROR loading courses!");
-           res.status(500).json({message: 'random error occurred.'});
-       }
-       else { */
-
     setTimeout(() => {
 
         res.status(200).json({ payload: Object.values(MOVIES) });
 
     }, 200);
-
-    //  }
 }
 
 
